084d4e64874e595df582cc2d88b6adc829e09662
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2020-07-18 04:15:06 -0400

    Add sanity check when withdrawing from account

--- UserFront/src/main/java/com/ctmhoang/userfront/service/impl/AccountServiceImpl.java
+++ UserFront/src/main/java/com/ctmhoang/userfront/service/impl/AccountServiceImpl.java
@@ -14,117 +14,138 @@ import java.security.Principal;
 import java.util.Date;
 
 @Service
-public class AccountServiceImpl implements IAccountService {
-  @Autowired private PrimaryAccountDao primaryAccountDao;
+public class AccountServiceImpl implements IAccountService
+{
+    @Autowired
+    private PrimaryAccountDao primaryAccountDao;
 
-  @Autowired private SavingsAccountDao savingsAccountDao;
+    @Autowired
+    private SavingsAccountDao savingsAccountDao;
 
-  @Autowired private IUserService userService;
+    @Autowired
+    private IUserService userService;
 
-  @Autowired private ITransactionService transactionService;
+    @Autowired
+    private ITransactionService transactionService;
 
-  private static int nextAccNum = 11223145;
+    private static int nextAccNum = 11223145;
 
-  @Override
-  public PrimaryAccount createPrimaryAccount() {
-    var primAcc = new PrimaryAccount();
-    primAcc.setAccBal(new BigDecimal(0.0));
-    primAcc.setAccNum(accGen());
-    primaryAccountDao.save(primAcc);
-    return primAcc;
-  }
-
-  @Override
-  public SavingsAccount createSavingsAccount() {
-    var saveAcc = new SavingsAccount();
-    saveAcc.setAccBal(new BigDecimal(0.0));
-    saveAcc.setAccNum(accGen());
-    savingsAccountDao.save(saveAcc);
-    return saveAcc;
-  }
-
-  @Override
-  public void deposit(String type, double parseDouble, Principal principal) {
-    User user = userService.findByUserName(principal.getName());
-    if (type.equalsIgnoreCase("Primary")) {
-      PrimaryAccount primaryAccount = user.getPrimAcc();
-      primaryAccount.setAccBal(primaryAccount.getAccBal().add(new BigDecimal(parseDouble)));
-      primaryAccountDao.save(primaryAccount);
-
-      Date date = new Date();
-
-      var primTrans =
-          new PrimaryTransaction(
-              parseDouble,
-              date,
-              "Deposit from primary account",
-              "Account",
-              "Finished",
-              primaryAccount.getAccBal(),
-              primaryAccount);
-      transactionService.savePrimaryDepositTransaction(primTrans);
-    } else if (type.equalsIgnoreCase("Savings")) {
-      SavingsAccount savingsAccount = user.getSaveAcc();
-      savingsAccount.setAccBal(savingsAccount.getAccBal().add(new BigDecimal(parseDouble)));
-      savingsAccountDao.save(savingsAccount);
-
-      Date date = new Date();
-
-      var saveTrans =
-          new SavingsTransaction(
-              parseDouble,
-              date,
-              "Deposit from savings account",
-              "Account",
-              "Finished",
-              savingsAccount.getAccBal(),
-              savingsAccount);
-
-      transactionService.saveSavingsDepositTransaction(saveTrans);
+    @Override
+    public PrimaryAccount createPrimaryAccount()
+    {
+        var primAcc = new PrimaryAccount();
+        primAcc.setAccBal(new BigDecimal(0.0));
+        primAcc.setAccNum(accGen());
+        primaryAccountDao.save(primAcc);
+        return primAcc;
     }
-  }
 
-  @Override
-  public void withdraw(String type, double parseDouble, Principal principal) {
-    User user = userService.findByUserName(principal.getName());
-    if (type.equalsIgnoreCase("Primary")) {
-      PrimaryAccount primaryAccount = user.getPrimAcc();
-      primaryAccount.setAccBal(primaryAccount.getAccBal().subtract(new BigDecimal(parseDouble)));
-      primaryAccountDao.save(primaryAccount);
-
-      Date date = new Date();
-
-      var primTrans =
-          new PrimaryTransaction(
-              parseDouble,
-              date,
-              "Withdraw from primary account",
-              "Account",
-              "Finished",
-              primaryAccount.getAccBal(),
-              primaryAccount);
-      transactionService.saveWithdrawDepositTransaction(primTrans);
-    } else if (type.equalsIgnoreCase("Savings")) {
-      SavingsAccount savingsAccount = user.getSaveAcc();
-      savingsAccount.setAccBal(savingsAccount.getAccBal().subtract(new BigDecimal(parseDouble)));
-      savingsAccountDao.save(savingsAccount);
-
-      Date date = new Date();
-
-      var saveTrans =
-          new SavingsTransaction(
-              parseDouble,
-              date,
-              "Withdraw from savings account",
-              "Account",
-              "Finished",
-              savingsAccount.getAccBal(),
-              savingsAccount);
-      transactionService.saveWithdrawDepositTransaction(saveTrans);
+    @Override
+    public SavingsAccount createSavingsAccount()
+    {
+        var saveAcc = new SavingsAccount();
+        saveAcc.setAccBal(new BigDecimal(0.0));
+        saveAcc.setAccNum(accGen());
+        savingsAccountDao.save(saveAcc);
+        return saveAcc;
     }
-  }
 
-  private int accGen() {
-    return ++nextAccNum;
-  }
+    @Override
+    public void deposit(String type, double parseDouble, Principal principal)
+    {
+        User user = userService.findByUserName(principal.getName());
+        if (type.equalsIgnoreCase("Primary"))
+        {
+            PrimaryAccount primaryAccount = user.getPrimAcc();
+            primaryAccount.setAccBal(primaryAccount.getAccBal().add(new BigDecimal(parseDouble)));
+            primaryAccountDao.save(primaryAccount);
+
+            Date date = new Date();
+
+            var primTrans =
+                    new PrimaryTransaction(
+                            parseDouble,
+                            date,
+                            "Deposit from primary account",
+                            "Account",
+                            "Finished",
+                            primaryAccount.getAccBal(),
+                            primaryAccount);
+            transactionService.savePrimaryDepositTransaction(primTrans);
+        }
+        else if (type.equalsIgnoreCase("Savings"))
+        {
+            SavingsAccount savingsAccount = user.getSaveAcc();
+            savingsAccount.setAccBal(savingsAccount.getAccBal().add(new BigDecimal(parseDouble)));
+            savingsAccountDao.save(savingsAccount);
+
+            Date date = new Date();
+
+            var saveTrans =
+                    new SavingsTransaction(
+                            parseDouble,
+                            date,
+                            "Deposit from savings account",
+                            "Account",
+                            "Finished",
+                            savingsAccount.getAccBal(),
+                            savingsAccount);
+
+            transactionService.saveSavingsDepositTransaction(saveTrans);
+        }
+    }
+
+    @Override
+    public void withdraw(String type, double parseDouble, Principal principal)
+    {
+        User user = userService.findByUserName(principal.getName());
+        if (type.equalsIgnoreCase("Primary"))
+        {
+            PrimaryAccount primaryAccount = user.getPrimAcc();
+
+            var tmp = primaryAccount.getAccBal().add(new BigDecimal(parseDouble));
+            if (tmp.compareTo(BigDecimal.ZERO) < 0) return;
+            primaryAccount.setAccBal(tmp);
+            primaryAccountDao.save(primaryAccount);
+
+            Date date = new Date();
+
+            var primTrans =
+                    new PrimaryTransaction(
+                            parseDouble,
+                            date,
+                            "Withdraw from primary account",
+                            "Account",
+                            "Finished",
+                            primaryAccount.getAccBal(),
+                            primaryAccount);
+            transactionService.saveWithdrawDepositTransaction(primTrans);
+        }
+        else if (type.equalsIgnoreCase("Savings"))
+        {
+            SavingsAccount savingsAccount = user.getSaveAcc();
+            var tmp = savingsAccount.getAccBal().add(new BigDecimal(parseDouble));
+            if (tmp.compareTo(BigDecimal.ZERO) < 0) return;
+            savingsAccount.setAccBal(tmp);
+            savingsAccountDao.save(savingsAccount);
+
+            Date date = new Date();
+
+            var saveTrans =
+                    new SavingsTransaction(
+                            parseDouble,
+                            date,
+                            "Withdraw from savings account",
+                            "Account",
+                            "Finished",
+                            savingsAccount.getAccBal(),
+                            savingsAccount);
+            transactionService.saveWithdrawDepositTransaction(saveTrans);
+        }
+    }
+
+    private int accGen()
+    {
+        return ++nextAccNum;
+    }
 }
